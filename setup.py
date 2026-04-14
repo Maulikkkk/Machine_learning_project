cf499c25e8cfb5f2fe4d3753dc35e5535875039f
Maulik Gupta <maulik.g29@gmail.com>
2026-04-14 22:34:31 +0530

    First setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+    return requirements
+
+
+setup(
+    name='ML_project',
+    version='0.0.1',
+    author='Maulik',
+    author_email='maulik.g29@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
